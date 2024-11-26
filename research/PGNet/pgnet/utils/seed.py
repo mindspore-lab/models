ba93f049a4193ea7e841058fb8c5c17208272df4
wxsIcey <1790571317@qq.com>
2024-11-26 19:17:06 +0800

    add utils module

---
+++ research/PGNet/pgnet/utils/seed.py
@@ -0,0 +1,17 @@
+"""random seed"""
+import random
+
+import numpy as np
+
+import mindspore as ms
+
+
+def set_seed(seed=42):
+    """
+    seed: seed int
+
+    Note: to ensure model init stability, rank_id is removed from seed.
+    """
+    random.seed(seed)
+    ms.set_seed(seed)
+    np.random.seed(seed)
