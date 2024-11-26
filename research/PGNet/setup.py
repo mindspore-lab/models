7a230f80f5643ac0423943cf1564cd4d15f352ef
wxsIcey <1790571317@qq.com>
2024-11-26 19:19:34 +0800

    add config module

---
+++ research/PGNet/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+exec(open("mindocr/version.py").read())
+
+
+def read_requirements(fps):
+    reqs = []
+    for fp in fps:
+        with open(fp) as f:
+            reqs.extend(f.readlines())
+
+    return reqs
+
+
+setup(
+    name="mindocr",
+    author="MindSpore Ecosystem",
+    author_email="mindspore-ecosystem@example.com",
+    url="https://github.com/mindspore-lab/mindocr",
+    project_urls={
+        "Sources": "https://github.com/mindspore-lab/mindocr",
+        "Issue Tracker": "https://github.com/mindspore-lab/mindocr/issues",
+    },
+    description="A toolbox of OCR models and algorithms based on MindSpore.",
+    license="Apache Software License 2.0",
+    include_package_data=True,
+    packages=find_packages(include=["mindocr", "mindocr.*"]),
+    install_requires=read_requirements(["requirements.txt"]),
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    version=__version__,
+    zip_safe=False,
+)
