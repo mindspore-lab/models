816cb635c76081e532ee7eceb95469e14a037038
XU Mingshi <91017482+mxuax@users.noreply.github.com>
2025-08-13 15:53:07 +0800

    Delete research/EdinU/beir/generation directory

--- research/EdinU/beir/generation/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from .generate import PassageExpansion, QueryGenerator
-
-__all__ = [
-    "PassageExpansion",
-    "QueryGenerator",
-]
--- research/EdinU/beir/generation/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from .auto_model import QGenModel
-from .tilde import TILDE
-
-__all__ = [
-    "QGenModel",
-    "TILDE",
-]
